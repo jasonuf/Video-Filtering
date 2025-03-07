4992e2114da3fa20292aa7fffa9bae6f530207a0
tacoJason <155794000+tacoJason@users.noreply.github.com>
2025-03-07 15:01:39 -0500

    creating child widgets, learning window properties

--- main.cpp
+++ main.cpp
@@ -3,12 +3,34 @@
 #include <QApplication>
 
 #include <QCheckBox>
+#include <QtWidgets>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QWidget window;
+    w.setWindowTitle("Example Name: HI!");
+
+    QPushButton *button = new QPushButton(
+        QApplication::translate("childwidget", "Press me"), &w);
+    button->move(100, 100);
+    button->show();
+
+    QCheckBox *checkbox = new QCheckBox("C&ase sensitive", &w);
+    checkbox->show();
+
+    QLabel *label = new QLabel(QApplication::translate("windowlayout", "Name:"));
+    QLineEdit *lineEdit = new QLineEdit();
+
+    QHBoxLayout *layout = new QHBoxLayout();
+    layout->addWidget(label);
+    layout->addWidget(lineEdit);
+    window.setLayout(layout);
+
+
     w.show();
 
     return a.exec();
+
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,22 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QCheckBox>
+#include <QFileDevice>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    QCheckBox *checkbox = new QCheckBox("C&ase sensitive", this);
+
+
+
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
+
 
     delete ui;
 }
