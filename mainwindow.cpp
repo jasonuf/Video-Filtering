441fb0649b1863781511bd87254a1e53e48cdadb
tacoJason <155794000+tacoJason@users.noreply.github.com>
2025-03-09 16:41:01 -0400

    Menu bar layout set up and file dialog functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,62 +7,23 @@
 #include <QMenu>
 #include <QDebug>
 #include <QVBoxLayout>
+#include <QFileDialog>
 
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    ,videoArea(new VideoPlayer(this))
     , ui(new Ui::MainWindow)
 {
+    ui->setupUi(this);
+    setFixedSize(960,540);
 
-    //setCentralWidget(videoArea);
+    videoOpen = new QString(QFileDialog::getOpenFileName(this,
+        tr("Open Image"), "./", tr("Image Files (*.png *.jpg *.bmp)")));
 
-    //createMenus();
-
-    // QWidget *widget = new QWidget;
-    // setCentralWidget(widget);
-
-    // QWidget *topFiller = new QWidget;
-    // topFiller->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    // infoLabel = new QLabel(tr("<i>Choose a menu option, or right-click to "
-    //                           "invoke a context menu</i>"));
-    // infoLabel->setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
-    // infoLabel->setAlignment(Qt::AlignCenter);
-    // infoLabel->setStyleSheet("QLabel { background-color : red; color : blue; }");
-
-
-    // QWidget *bottomFiller = new QWidget;
-    // bottomFiller->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    // QVBoxLayout *layout = new QVBoxLayout;
-    // layout->setContentsMargins(5, 5, 5, 5);
-    // layout->addWidget(topFiller);
-    // layout->addWidget(infoLabel);
-    // layout->addWidget(bottomFiller);
-    // widget->setLayout(layout);
-
-
-    // createActions();
-    // createMenus();
-
-    // QString message = tr("A context menu is available by right-clicking");
-    // statusBar()->showMessage(message);
-
-    // setWindowTitle(tr("Menus"));
-    // setMinimumSize(160, 160);
-    // resize(480, 320);
-
-    fileMenu = new QMenu("View");
-    fileMenu->addAction("Sky");
-    fileMenu->addAction("Cloud");
-    fileMenu->addAction("Tree");
-
-
-
+    qInfo() << "The property videoOpen is set to: " << *videoOpen;
     qInfo() << "Main Window Created";
 
-    ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -80,39 +41,7 @@ void MainWindow::actionTest()
 void MainWindow::createMenus()
 {
 
-    QMenu* saveAsMenu = new QMenu(tr("&Save As"), this);
-    // for (QAction *action : std::as_const(saveAsActs))
-    //     saveAsMenu->addAction(action);
 
-    QMenu* fileMenu = new QMenu(tr("&File"), this);
-    // fileMenu->addAction(openAct);
-    // fileMenu->addMenu(saveAsMenu);
-    // fileMenu->addAction(printAct);
-    // fileMenu->addSeparator();
-    // fileMenu->addAction(exitAct);
-
-    QMenu* optionMenu = new QMenu(tr("&Options"), this);
-    // optionMenu->addAction(penColorAct);
-    // optionMenu->addAction(penWidthAct);
-    // optionMenu->addSeparator();
-    // optionMenu->addAction(clearScreenAct);
-
-    QMenu* helpMenu = new QMenu(tr("&Help"), this);
-    // helpMenu->addAction(aboutAct);
-    // helpMenu->addAction(aboutQtAct);
-
-    menuBar()->addMenu(fileMenu);
-    menuBar()->addMenu(optionMenu);
-    menuBar()->addMenu(helpMenu);
 }
 
-
-void MainWindow::createActions()
-{
-    newAct = new QAction(QIcon::fromTheme(QIcon::ThemeIcon::DocumentNew),
-                         tr("&New"), this);
-    newAct->setShortcuts(QKeySequence::New);
-    newAct->setStatusTip(tr("Create a new file"));
-    connect(newAct, &QAction::triggered, this, &MainWindow::actionTest);
-}
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
+
 #include "videoplayer.h"
+
+#include <QMainWindow>
 #include <QLabel>
+#include <QAction>
+#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -26,62 +30,24 @@ public slots:
 
 
 private slots:
-    // void newFile();
-    // void open();
-    // void save();
-    // void print();
-    // void undo();
-    // void redo();
-    // void cut();
-    // void copy();
-    // void paste();
-    // void bold();
-    // void italic();
-    // void leftAlign();
-    // void rightAlign();
-    // void justify();
-    // void center();
-    // void setLineSpacing();
-    // void setParagraphSpacing();
-    // void about();
-    // void aboutQt();
+
+
 
 private:
     Ui::MainWindow *ui;
-    VideoPlayer* videoArea;
+    VideoPlayer* mainVideo;
 
     void createMenus();
     void createActions();
 
+    QString* videoOpen;
 
 
 
-    QMenu *fileMenu;
-    QMenu *editMenu;
-    QMenu *formatMenu;
-    QMenu *helpMenu;
-    QActionGroup *alignmentGroup;
-    QAction *newAct;
-    QAction *openAct;
-    QAction *saveAct;
-    QAction *printAct;
-    QAction *exitAct;
-    QAction *undoAct;
-    QAction *redoAct;
-    QAction *cutAct;
-    QAction *copyAct;
-    QAction *pasteAct;
-    QAction *boldAct;
-    QAction *italicAct;
-    QAction *leftAlignAct;
-    QAction *rightAlignAct;
-    QAction *justifyAct;
-    QAction *centerAct;
-    QAction *setLineSpacingAct;
-    QAction *setParagraphSpacingAct;
-    QAction *aboutAct;
-    QAction *aboutQtAct;
-    QLabel *infoLabel;
+
+
+
+
 
 };
 #endif // MAINWINDOW_H
